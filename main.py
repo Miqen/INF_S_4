6eda5e03323e3fef30eee36820388a03c9782c99
Miqen <ArkadiusZielinski@o2.pl>
2023-04-21 18:53:23 +0200

    define class methods

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 class Coordinates_transformation:
-    pass
+    
+    def xyz2blh():
+        pass
+    
+    def blh2xyz():
+        pass
+    
+    def xyz2neu():
+        pass
+    
+    def bl2xyz2000():
+        pass
+    
+    def bl2xyz1992():
+        pass
+
 
 
 
